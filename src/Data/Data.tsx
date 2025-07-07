b4c637dd9d1769609b7092f858f942afea53c2f8
makwanaavi <makwanaavi73@gmail.com>
2025-07-07 17:22:39 +0530

    #update

---
+++ src/Data/Data.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const companies = [
+  "Google",
+  "Amazon",
+  "Figma",
+  "Meta",
+  "Microsoft",
+  "Netflix",
+  "Oracle",
+  "Pinterest",
+  "Slack",
+  "Spotify",
+  "Walmart",
+];
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from '../Header/Header'
 import DreamJob from '../LandingPage/DreamJob'
+import Companies from '../LandingPage/Companies'
 
 const HomePage = () => {
   return (
     <div className='min-h-[100vh] bg-mine-shaft-950 font-["poppins"]'>
         <Header />
         <DreamJob/>
-        
+        <Companies/>
     </div>
   )
 }
