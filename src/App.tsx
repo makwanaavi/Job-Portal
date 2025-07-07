c8ac72e2f2d82d5955178fab8593edf8ba7c76b5
makwanaavi <makwanaavi73@gmail.com>
2025-07-07 10:59:16 +0530

    #update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MantineProvider} from '@mantine/core';
+import "./App.css"
+import '@mantine/core/styles.css'
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider>
+      <h1>Welcome to Mantine!</h1>
+    </MantineProvider>
   );
 }
-
-export default App;
