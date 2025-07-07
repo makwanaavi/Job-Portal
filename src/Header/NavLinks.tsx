e434dbd4957e52c22ada9153304c77901e2eaed1
makwanaavi <makwanaavi73@gmail.com>
2025-07-07 13:56:23 +0530

    #update

--- src/Header/NavLinks.tsx
+++ src/Header/NavLinks.tsx
@@ -1,23 +1,53 @@
+/* eslint-disable no-lone-blocks */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 const NavLinks = () => {
+  const Links = [
+    { name: "Find Jobs", url: "find-jobs" },
+    { name: "Find Talent", url: "find-talent" },
+    { name: "Upload Jobs", url: "upload-jobs" },
+    { name: "About Us", url: "about-us" },
+  ];
   return (
     <div className="flex gap-12">
-           <a href="#" className="hover:text-bright-sun-400 active:scale-125 transition-all">
-             Find Job
-           </a>
-           <a href="#" className="hover:text-bright-sun-400 active:scale-125 transition-all">
-             Find Talent
-           </a>
-           <a href="#" className="hover:text-bright-sun-400 active:scale-125 transition-all">
-             Upload Jobs
-           </a>
-           <a href="#" className="hover:text-bright-sun-400 active:scale-125 transition-all">
-             About Us
-           </a>
-         </div>
-  )
-}
+      {Links.map((link, index) => (
+        <div>
+          <Link key={index} to={link.url}>
+            {link.name}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default NavLinks
+export default NavLinks;
+
+{
+  /* <a
+        href="#"
+        className="hover:text-bright-sun-400 active:scale-125 transition-all"
+      >
+        Find Job
+      </a>
+      <a
+        href="#"
+        className="hover:text-bright-sun-400 active:scale-125 transition-all"
+      >
+        Find Talent
+      </a>
+      <a
+        href="#"
+        className="hover:text-bright-sun-400 active:scale-125 transition-all"
+      >
+        Upload Jobs
+      </a>
+      <a
+        href="#"
+        className="hover:text-bright-sun-400 active:scale-125 transition-all"
+      >
+        About Us
+      </a>    */
+}
