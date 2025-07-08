04881e04d1bd3a82f35f461b1957fc00f6517b80
makwanaavi <makwanaavi73@gmail.com>
2025-07-08 11:11:46 +0530

    #update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,6 +7,7 @@ import Works from '../LandingPage/Works'
 import Testimonials from '../LandingPage/Testimonials'
 import Subscribe from '../LandingPage/Subscribe'
 import Footer from '../LandingPage/Footer'
+import CopyRight from '../LandingPage/CopyRight'
 
 const HomePage = () => {
   return (
@@ -19,6 +20,7 @@ const HomePage = () => {
         <Testimonials/>
         <Subscribe />
         <Footer/>
+        <CopyRight/>
     </div>
   )
 }
